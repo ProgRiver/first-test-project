b850011ad66623f457435c5fdaf8268b4c0674fa
ProgRiver <barkas.ssr@mail.ru>
2022-03-19 16:13:26 +0300

    Control, setup and verification of tests

--- test_main_page.py
+++ test_main_page.py
@@ -4,34 +4,6 @@ from pages.login_page import LoginPage
 from pages.basket_page import BasketPage
 
 
-# def test_guest_can_go_to_login_page(browser):
-#     link = "http://selenium1py.pythonanywhere.com/"
-#     page = MainPage(browser, link)      # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
-#     page.open()                         # открываем страницу
-#     page.should_be_login_link()         # проверить наличие ссылки
-#     page.go_to_login_page()             # переход на страницу логина
-#     login_page = LoginPage(browser, browser.current_url)
-#     login_page.should_be_login_page()
-
-
-# def test_guest_should_see_login_link(browser):
-#     link = "http://selenium1py.pythonanywhere.com/"
-#     page = MainPage(browser, link)
-#     page.open()
-#     page.should_be_login_link()
-
-
-# def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
-#     link = "http://selenium1py.pythonanywhere.com/"
-#     page = MainPage(browser, link)
-#     page.open()
-#     page.should_be_go_to_basket_page()
-#     page.go_to_basket_page()
-#     page_basket = BasketPage(browser, browser.current_url)
-#     page_basket.sould_be_not_product_in_basket()
-#     page_basket.sould_be_empty_basket()
-
-
 @pytest.mark.login_guest
 class TestLoginFromMainPage():
     def test_guest_can_go_to_login_page(self, browser):
@@ -42,10 +14,22 @@ class TestLoginFromMainPage():
         page.go_to_login_page()
         login_page = LoginPage(browser, browser.current_url)
         login_page.should_be_login_page()
-    
+
 
     def test_guest_should_see_login_link(self, browser):
         link = "http://selenium1py.pythonanywhere.com/"
         page = MainPage(browser, link)
         page.open()
-        page.should_be_login_link()    
+        page.should_be_login_link() 
+
+
+def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_go_to_basket_page()
+    page.go_to_basket_page()
+    page_basket = BasketPage(browser, browser.current_url)
+    page_basket.sould_be_not_product_in_basket()
+    page_basket.sould_be_empty_basket()
+  
--- test_product_page.py
+++ test_product_page.py
@@ -4,70 +4,22 @@ from pages.product_page import ProductPage
 from pages.basket_page import BasketPage
 from pages.login_page import LoginPage
 
-# link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer"
-# links_list = [link + str(i) for i in range(10)]
-# links_list[7] = pytest.param(links_list[7], marks=pytest.mark.xfail(reason="[!] the page on link has a bug [!]"))
 
-# link_prod = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
-
-
-# @pytest.mark.skip(reason="[-] Test skip [-]")
-# @pytest.mark.parametrize('url', links_list)
-# def test_guest_can_add_product_to_basket(browser, url):
-#     page = ProductPage(browser, url)
-#     page.open()
-#     page.should_be_button_basket()
-#     page.click_btn_add_to_basket()
-#     page.solve_quiz_and_get_code()
-#     page.should_be_correct_name_product()
-#     page.should_be_correct_price()
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer"
+links_list = [link + str(i) for i in range(10)]
+links_list[7] = pytest.param(links_list[7], marks=pytest.mark.xfail(reason="[!] the page on link has a bug [!]"))
 
 
-# @pytest.mark.xfail
-# def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
-#     prod = ProductPage(browser, link_prod)
-#     prod.open()
-#     prod.click_btn_add_to_basket()
-#     prod.should_not_be_success_message()
-
-
-# def test_guest_cant_see_success_message(browser):
-#     prod = ProductPage(browser, link_prod)
-#     prod.open()
-#     prod.should_not_be_success_message()
-
-
-# @pytest.mark.xfail
-# def test_message_disappeared_after_adding_product_to_basket(browser):
-#     prod = ProductPage(browser, link_prod)
-#     prod.open()
-#     prod.click_btn_add_to_basket()
-#     prod.should_be_disappeared()
-
-
-# def test_guest_should_see_login_link_on_product_page(browser):
-#     link_log = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
-#     page = ProductPage(browser, link_log)
-#     page.open()
-#     page.should_be_login_link()
-
-
-# def test_guest_can_go_to_login_page_from_product_page(browser):
-#     link_log = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
-#     page = ProductPage(browser, link_log)
-#     page.open()
-#     page.go_to_login_page()
-
-
-# def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
-#     prod_page = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/studyguide-for-counter-hack-reloaded_205/"
-#     page = ProductPage(browser, prod_page)
-#     page.open()
-#     page.should_be_go_to_basket_page()
-#     page.go_to_basket_page()
-#     page_basket = BasketPage(browser, browser.current_url)
-#     page_basket.sould_be_not_product_in_basket()
-#     page_basket.sould_be_empty_basket()
+@pytest.mark.need_review
+@pytest.mark.parametrize('url', links_list)
+def test_guest_can_add_product_to_basket(browser, url):
+    page = ProductPage(browser, url)
+    page.open()
+    page.should_be_button_basket()
+    page.click_btn_add_to_basket()
+    page.solve_quiz_and_get_code()
+    page.should_be_correct_name_product()
+    page.should_be_correct_price()
 
 
 class TestUserAddToBasketFromProductPage():
@@ -81,7 +33,7 @@ class TestUserAddToBasketFromProductPage():
         self.reg.register_new_user(email, password)
         self.reg.should_be_authorized_user()
 
-
+    
     def test_user_cant_see_success_message(self, browser):
         link2 = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
         prod2 = ProductPage(browser, link2)
@@ -89,6 +41,7 @@ class TestUserAddToBasketFromProductPage():
         prod2.should_not_be_success_message()
     
 
+    @pytest.mark.need_review
     def test_user_can_add_product_to_basket(self, browser):
         link3 = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
         page2 = ProductPage(browser, link3)
@@ -98,3 +51,54 @@ class TestUserAddToBasketFromProductPage():
         page2.should_be_correct_name_product()
         page2.should_be_correct_price()
 
+
+@pytest.mark.xfail
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    link_prod = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
+    prod = ProductPage(browser, link_prod)
+    prod.open()
+    prod.click_btn_add_to_basket()
+    prod.should_not_be_success_message()
+
+
+def test_guest_cant_see_success_message(browser):
+    link_prod = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
+    prod = ProductPage(browser, link_prod)
+    prod.open()
+    prod.should_not_be_success_message()
+
+
+@pytest.mark.xfail
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    link_prod = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
+    prod = ProductPage(browser, link_prod)
+    prod.open()
+    prod.click_btn_add_to_basket()
+    prod.should_be_disappeared()
+
+
+def test_guest_should_see_login_link_on_product_page(browser):
+    link_log = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link_log)
+    page.open()
+    page.should_be_login_link()
+
+
+@pytest.mark.need_review
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link_log = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link_log)
+    page.open()
+    page.go_to_login_page()
+
+
+@pytest.mark.need_review
+def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
+    prod_page = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/studyguide-for-counter-hack-reloaded_205/"
+    page = ProductPage(browser, prod_page)
+    page.open()
+    page.should_be_go_to_basket_page()
+    page.go_to_basket_page()
+    page_basket = BasketPage(browser, browser.current_url)
+    page_basket.sould_be_not_product_in_basket()
+    page_basket.sould_be_empty_basket()
